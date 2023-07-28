323e2393da53a0f34a6ca6c18a703e840f977638
Erishen <lei_sun@trip.com>
2023-07-28 11:29:31 +0800

    update

--- app/footer.js
+++ app/footer.js
@@ -1,9 +1,16 @@
 import '@/styles/footer.scss'
+import Link from 'next/link'
 
 export default function Footer() {
     return (
       <footer>
-        <div>footer</div>
+        <div className='wrapper'>
+          <Link href="/footer1" className='link'>Footer1</Link>
+          <Link href="/footer2" className='link'>Footer2</Link>
+          <Link href="/footer3" className='link'>Footer3</Link>
+          <Link href="/footer4" className='link'>Footer4</Link>
+          <Link href="/footer5" className='link'>Footer5</Link>
+        </div>
       </footer>
     )
 }
--- app/header.js
+++ app/header.js
@@ -1,9 +1,16 @@
 import '@/styles/header.scss'
+import Link from 'next/link'
 
 export default function Header() {
     return (
       <header>
-        <nav>header</nav>
+        <nav>
+          <Link href="/menu1" className='link'>Menu1</Link>
+          <Link href="/menu2" className='link'>Menu2</Link>
+          <Link href="/menu3" className='link'>Menu3</Link>
+          <Link href="/menu4" className='link'>Menu4</Link>
+          <Link href="/menu5" className='link'>Menu5</Link>
+        </nav>
       </header>
     )
 }
